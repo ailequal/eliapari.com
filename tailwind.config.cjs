ac02d31cf3064549dbe8f70bf7cda0b58f424668
Elia Pari <37016865+ailequal@users.noreply.github.com>
2023-09-14 00:30:28 +0200

    started working on custom colors and updated formatted code

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  // TODO: Move the colors object outside the extend section when the default one are not needed anymore.
   theme: {
     extend: {
       screens: {
         xsm: "420px",
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        ["ae-black"]: "#515151",
+        ["ae-white"]: "#E8E6DF",
+        ["ae-red"]: "#F2777A",
+        ["ae-blue"]: "#6699CC",
+        ["ae-pink"]: "#CC99CC",
       },
     },
   },
