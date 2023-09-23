3c2156b3b645bf927717a400364c5b16b3f97e94
Elia Pari <37016865+ailequal@users.noreply.github.com>
2023-09-23 10:24:35 +0200

    implemented readingTime for blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.eliapari.com",
-  // https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-themes
-  markdown: { shikiConfig: { theme: "dark-plus" } },
+  markdown: {
+    remarkPlugins: [remarkReadingTime], // https://docs.astro.build/en/recipes/reading-time
+    shikiConfig: { theme: "dark-plus" }, // https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-themes
+  },
   integrations: [mdx(), sitemap(), tailwind()],
 });
