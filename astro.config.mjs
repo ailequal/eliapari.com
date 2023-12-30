84f8a7e7dfdf928fa448f1146b01d9166712e128
Elia Pari <37016865+ailequal@users.noreply.github.com>
2023-12-30 12:30:32 +0100

    removed mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
@@ -11,5 +10,5 @@ export default defineConfig({
     remarkPlugins: [remarkReadingTime], // https://docs.astro.build/en/recipes/reading-time
     shikiConfig: { theme: "dark-plus" }, // https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-themes
   },
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind()],
 });
